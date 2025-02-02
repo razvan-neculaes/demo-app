989e6a571ae864b4c9badd46260ea0adfac1c5cf
Razvan Neculaes <45574980+razvan-neculaes@users.noreply.github.com>
2025-02-02 13:33:47 +0200

    The Header Banner
    update
    
    update

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import 'jquery';
+import '@popperjs/core';
+
+// Import your custom SCSS
+import './utils/fonts.scss'
+import './page.scss';
